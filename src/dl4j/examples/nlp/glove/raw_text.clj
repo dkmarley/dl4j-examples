044fe989599daa9ab2147009ca1fb2b8f211468c
dkmarley <david.marley@adb.co.uk>
2017-07-25 17:42:48 +0100

    Change default GloVe learning rate from 0.1 to 0.09

--- src/dl4j/examples/nlp/glove/raw_text.clj
+++ src/dl4j/examples/nlp/glove/raw_text.clj
@@ -2,8 +2,9 @@
   (:require [dl4j.examples.util :as u])
   (:import (org.deeplearning4j.models.glove Glove$Builder)))
 
-;;;; There appears to be a problem with the GloVe implementation. Training fails frequently with a Nan error as described
-;;;; here: https://github.com/deeplearning4j/dl4j-examples/issues/227
+;;;; There appears to be a problem with the GloVe implementation. Training fails frequently with a NaN error as described
+;;;; here: https://github.com/deeplearning4j/dl4j-examples/issues/227. Decreasing the learning rate from 0.1 seems
+;;;; to significantly decrease the frequency of this on my machine.
 
 (defn vectors [iterator]
   (doto
@@ -11,7 +12,7 @@
         (.iterate iterator)
         (.tokenizerFactory (u/basic-tokenizer))
         (.alpha 0.75)
-        (.learningRate 0.1)
+        (.learningRate 0.099)
         (.epochs 25)
         (.xMax 100)
         (.batchSize 1000)
